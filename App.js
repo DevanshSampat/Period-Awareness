3ab49a5cd5bc774a50feb57c7f488a2413f995d7
Adnan Ahmed <adnanahmed.indian@gmail.com>
2022-03-05 19:10:46 +0530

    Connected to Atlas DB

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/connectDB");
+const dotenv = require("dotenv");
 
 const expressApp = express();
 expressApp.use(express.json());
-expressApp.use(express.urlencoded({extended: false}));
+expressApp.use(express.urlencoded({ extended: false }));
+
+dotenv.config();
+connectDB();
 
 expressApp.listen(parseInt(5000), "0.0.0.0", () => console.log(`Express Server started at port ${5000}`));
