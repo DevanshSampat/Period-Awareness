77b794242c5a0bde719308d175c230b3ca172d5f
AyushTurakhia <57479395+AyushTurakhia@users.noreply.github.com>
2022-03-06 01:07:57 +0530

    COmplete Quiz
    Done finish khatam sojao

--- app/src/main/java/com/period/awareness/Fragment4.java
+++ app/src/main/java/com/period/awareness/Fragment4.java
@@ -2,23 +2,31 @@ package com.period.awareness;
 
 import android.animation.Animator;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 public class Fragment4 extends Fragment {
     private TextView option1,option2,option3;
     private androidx.cardview.widget.CardView option1card,option2card,option3card,next,hint;
     private String correctanswer,selected;
+    private Animation wronganswer;
     private LinearLayout Quizlinearlayout;
     private TextView quesnum,anshint,question;
     private String[] correct,options,questions,faqs;
@@ -52,6 +60,8 @@ public class Fragment4 extends Fragment {
         quesnum.setText(getString(R.string.question_number,1,5));
         shownextques();
         hint.setVisibility(View.INVISIBLE);
+        wronganswer= AnimationUtils.loadAnimation(getContext(),R.anim.wrong_answer_animation);
+        wronganswer.setRepeatCount(1);
         return view;
     }
     private View.OnClickListener listen=new View.OnClickListener(){
@@ -61,13 +71,6 @@ public class Fragment4 extends Fragment {
             int a=view.getId();
             if(view.getId()==R.id.next){
                 if(selected.equals("")){
-                    return;
-                }
-                if(counter1==questions.length+1){
-                    //end quiz and reset
-                    
-                    counter1=0;
-                    optioncounter=0;
                     return;
                 }
                 else {
@@ -81,10 +84,26 @@ public class Fragment4 extends Fragment {
                             anshint.setText("Wrong \n"+correctanswer+"\n"+faqs[counter1-1]);
                         }
                         hint.setVisibility(View.VISIBLE);
+                        hint.startAnimation(wronganswer);
                         enter=false;
                     }
                     else if(counter1==questions.length){
                         //end quiz and reset
+                        Dialog dialog=new Dialog(getContext());
+                        dialog.setContentView(R.layout.dailog_score);
+                        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                        ((TextView)dialog.findViewById(R.id.result)).setText("Congrats you have Scored "+score+" out of 5");
+                        dialog.show();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                try{
+                                    dialog.dismiss();
+                                }catch (Exception ignored){
+                                }
+
+                            }
+                        },5000);
                         counter1=0;
                         optioncounter=0;
                         return;
@@ -132,6 +151,7 @@ public class Fragment4 extends Fragment {
         question.setText(questions[counter1]);
         correctanswer=correct[counter1];
         if(n[counter1]==3){
+            option3card.setVisibility(View.VISIBLE);
             option1.setText(options[optioncounter]);
             optioncounter++;
             option2.setText(options[optioncounter]);
