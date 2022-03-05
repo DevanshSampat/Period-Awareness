459444567735edd8f3a9a85256f4266c9c16bcad
DEVANSH <devansh.sampat@gmail.com>
2022-03-05 15:02:18 +0530

    Navigation bar added

---
+++ app/src/main/java/com/period/awareness/Fragment3.java
@@ -0,0 +1,18 @@
+package com.period.awareness;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return LayoutInflater.from(container.getContext()).inflate(R.layout.fragment3,container,false);
+    }
+}
--- app/src/main/java/com/period/awareness/MainActivity.java
+++ app/src/main/java/com/period/awareness/MainActivity.java
@@ -1,8 +1,13 @@
 package com.period.awareness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        fragmentTransaction.replace(R.id.host_fragment,new Fragment1());
+        fragmentTransaction.commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.page1:{
+                        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+                        fragmentTransaction.replace(R.id.host_fragment,new Fragment1());
+                        break;
+                    }
+                    case R.id.page2:{
+                        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                        fragmentTransaction.replace(R.id.host_fragment,new Fragment2());
+                        break;
+                    }
+                    case R.id.page3:{
+                        bottomNavigationView.getMenu().getItem(2).setChecked(true);
+                        fragmentTransaction.replace(R.id.host_fragment,new Fragment3());
+                        break;
+                    }
+                    case R.id.page4:{
+                        bottomNavigationView.getMenu().getItem(3).setChecked(true);
+                        fragmentTransaction.replace(R.id.host_fragment,new Fragment4());
+                        break;
+                    }
+                }
+                fragmentTransaction.commit();
+                return false;
+            }
+        });
     }
 }
