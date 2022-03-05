401988e6f5a72073797cf8ee70cdd634407b0a3a
aryan <aryanpathare281@gmail.com>
2022-03-05 18:07:08 +0530

    Connected to db

---
+++ Api/SchoolPostsApi.js
@@ -0,0 +1,5 @@
+const router = new require("express").Router();
+
+
+
+module.exports.router = router;
--- App.js
+++ App.js
@@ -4,4 +4,8 @@ const expressApp = express();
 expressApp.use(express.json());
 expressApp.use(express.urlencoded({extended: false}));
 
+expressApp.use("/posts/school", require("./Api/SchoolPostsApi").router);
+
 expressApp.listen(parseInt(5000), "0.0.0.0", () => console.log(`Express Server started at port ${5000}`));
+
+require("./Config/Config");
