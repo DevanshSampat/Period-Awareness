fffe5006c245218440a33b623874695d96adb2f5
DEVANSH <devansh.sampat@gmail.com>
2022-03-06 13:24:10 +0530

    Chat added

--- app/src/main/java/com/period/awareness/RestApi.java
+++ app/src/main/java/com/period/awareness/RestApi.java
@@ -1,11 +1,19 @@
 package com.period.awareness;
 
+import com.period.awareness.datamodels.CreatorData;
 import com.period.awareness.datamodels.EntirePostModel;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface RestApi {
     @GET("/posts/all")
     Call<EntirePostModel> getPostModel();
+
+    @POST("/posts/create")
+    Call<ResponseBody> createPost(@Body CreatorData creatorData);
 }
