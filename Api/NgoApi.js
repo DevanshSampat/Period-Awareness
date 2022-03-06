88c845da2edb2ff8958b2045679856f00a56c651
aryan <aryanpathare281@gmail.com>
2022-03-06 10:09:37 +0530

    Added ngo contact endpoints

--- Api/NgoApi.js
+++ Api/NgoApi.js
@@ -1,7 +1,9 @@
-const { registerNgo, getNgosByLocation} = require("../Controllers/NgoController");
+const { registerNgo, getNgosByLocation, contactNgo, getRequests} = require("../Controllers/NgoController");
 const router = new require("express").Router();
 
 router.post("/register", registerNgo);
 router.get("/getngo", getNgosByLocation);
+router.post("/contact", contactNgo);
+router.get("/requests/:id", getRequests);
 
 module.exports.router = router;
