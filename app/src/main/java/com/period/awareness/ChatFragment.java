2cf061ff5d039b74163acea8b51ab8e66b073c92
DEVANSH <devansh.sampat@gmail.com>
2022-03-06 13:24:10 +0530

    Chat added

--- app/src/main/java/com/period/awareness/ChatFragment.java
+++ app/src/main/java/com/period/awareness/ChatFragment.java
@@ -8,7 +8,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.period.awareness.adapters.PostAdapter;
+import com.period.awareness.datamodels.CreatorData;
+import com.period.awareness.datamodels.EntirePostModel;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class ChatFragment extends Fragment {
@@ -19,6 +31,33 @@ public class ChatFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_chat, container, false);
+        v.findViewById(R.id.submit_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreatorData creatorData = new CreatorData();
+                creatorData.setQuestion(((EditText)v.findViewById(R.id.question)).getText().toString());
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(getString(R.string.base_url))
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                RestApi restApi = retrofit.create(RestApi.class);
+                Call<ResponseBody> call = restApi.createPost(creatorData);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if(response.isSuccessful() && response.body()!=null) {
+                            Toast.makeText(getContext(), "Question Submitted", Toast.LENGTH_SHORT).show();
+                            ((EditText)v.findViewById(R.id.question)).setText("");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
         Bundle bundle= this.getArguments();
         String student_id,teacher_id;
         if(bundle!=null){
