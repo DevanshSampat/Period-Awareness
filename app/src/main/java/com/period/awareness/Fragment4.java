909a9accf7e460e9269f3fa4ed7810b5b2734aaa
DEVANSH <devansh.sampat@gmail.com>
2022-03-06 12:53:03 +0530

    Quiz added

--- app/src/main/java/com/period/awareness/Fragment4.java
+++ app/src/main/java/com/period/awareness/Fragment4.java
@@ -11,6 +11,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.period.awareness.adapters.PostAdapter;
+import com.period.awareness.datamodels.EntirePostModel;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Fragment4 extends Fragment  implements View.OnClickListener {
     @Nullable
@@ -19,6 +30,29 @@ public class Fragment4 extends Fragment  implements View.OnClickListener {
         View v=  LayoutInflater.from(container.getContext()).inflate(R.layout.fragment4,container,false);
         Button go_to_chat = v.findViewById(R.id.go_to_chat);
         go_to_chat.setOnClickListener(this);
+        RecyclerView recyclerView = v.findViewById(R.id.rv_post);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new PostAdapter());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RestApi restApi = retrofit.create(RestApi.class);
+        Call<EntirePostModel> call = restApi.getPostModel();
+        call.enqueue(new Callback<EntirePostModel>() {
+            @Override
+            public void onResponse(Call<EntirePostModel> call, Response<EntirePostModel> response) {
+                if(response.isSuccessful() && response.body()!=null) {
+                    ((PostAdapter)recyclerView.getAdapter()).setPostModelArrayList(response.body().getPostModelArrayList());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<EntirePostModel> call, Throwable t) {
+
+            }
+        });
         return v;
     }
     @Override
--- app/src/main/java/com/period/awareness/RestApi.java
+++ app/src/main/java/com/period/awareness/RestApi.java
@@ -1,7 +1,11 @@
 package com.period.awareness;
 
+import com.period.awareness.datamodels.EntirePostModel;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RestApi {
-
+    @GET("/posts/all")
+    Call<EntirePostModel> getPostModel();
 }
